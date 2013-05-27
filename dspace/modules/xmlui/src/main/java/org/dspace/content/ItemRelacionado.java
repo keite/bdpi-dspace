ad22348af4bb73a9689d6b938f6df97c7e0dc1e1
DspaceTeam <danielshinsky@gmail.com>
2013-05-27 14:26:54 -0300

    Alterado tipo de atributo ano para String

--- dspace/modules/xmlui/src/main/java/org/dspace/content/ItemRelacionado.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/content/ItemRelacionado.java
@@ -2,26 +2,26 @@ package org.dspace.content;
 
 public class ItemRelacionado {
 	
-	private int ano;
+	private String ano;
 	private String tipo;
 	private String titulo;
-        private String handle;
+	private String handle;
 
-        public ItemRelacionado(){}
+	public ItemRelacionado(){}
 	
-	public ItemRelacionado(int ano, String tipo, String titulo, String handle) {
+	public ItemRelacionado(String ano, String tipo, String titulo, String handle) {
 		super();
 		this.ano = ano;
 		this.tipo = tipo;
 		this.titulo = titulo;
-                this.handle = handle;
+		this.handle = handle;
 	}
 	
-	public int getAno() {
+	public String getAno() {
 		return ano;
 	}
 	
-	public void setAno(int ano) {
+	public void setAno(String ano) {
 		this.ano = ano;
 	}
 	
@@ -41,12 +41,12 @@ public class ItemRelacionado {
 		this.titulo = titulo;
 	}
 
-        public String getHandle() {
-                return handle;
-        }
+	public String getHandle() {
+			return handle;
+	}
 
-        public void setHandle(String handle) {
-                this.handle = handle;
-        }
+	public void setHandle(String handle) {
+			this.handle = handle;
+	}
 }
 
